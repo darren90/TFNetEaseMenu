9bccdafd46e8a5c1a162e08dbd5d082ed08913db
darren90 <1005052145@qq.com>
2015-12-13 22:06:38 +0800

    add something

--- TFNetEaseMenu/TFNetEaseMenu/PopUpAnimation.m
+++ TFNetEaseMenu/TFNetEaseMenu/PopUpAnimation.m
@@ -10,12 +10,19 @@
 
 @implementation PopUpAnimation
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    return self;
 }
-*/
+
+-(void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    
+}
 
 @end
--- TFNetEaseMenu/TFNetEaseMenu/ViewController.m
+++ TFNetEaseMenu/TFNetEaseMenu/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
-
+#import <pop/POP.h>
 @interface ViewController ()
+
+@property (nonatomic,weak)UIButton * anmationView;
 
 @end
 
@@ -17,12 +19,41 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"" style:UIBarButtonItemStyleDone target:self action:@selector(jumpToClassify)];
+    
+    UIButton * anmationView = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.anmationView = anmationView;
+    [self.view addSubview:anmationView];
+    anmationView.frame = CGRectMake((self.view.frame.size.width - 100)/2, 200, 100, 100);
+    [anmationView setBackgroundImage:[UIImage imageNamed:@"ic_01"] forState:UIControlStateNormal];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"动画" style:UIBarButtonItemStyleDone target:self action:@selector(jumpToClassify)];
 }
 
 -(void)jumpToClassify
 {
     NSLog(@"");
+    [self performPopUpAnimation];
+}
+
+
+#pragma mark 需要的效果
+-(void)performPopUpAnimation
+{
+    [self.anmationView pop_removeAllAnimations];
+    POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+    anim.springBounciness = 20;//10;
+    anim.springSpeed = 20;
+    anim.fromValue = [NSValue valueWithCGPoint:CGPointMake(0, 0)];
+    anim.toValue = [NSValue valueWithCGPoint:CGPointMake(1.0, 1.0)];
+    
+    POPBasicAnimation *opacityAnim = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerOpacity];
+    
+    opacityAnim.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    opacityAnim.duration = 0.3;
+    opacityAnim.toValue = @1.0;
+    
+    [self.anmationView.layer pop_addAnimation:anim forKey:@"AnimationScale"];
+    [self.anmationView.layer pop_addAnimation:opacityAnim forKey:@"AnimateOpacity"];
 }
 
 
